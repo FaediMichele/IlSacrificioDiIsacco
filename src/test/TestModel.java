339b456cd89240a26dddba875d007d14a31482a8
Michele Faedi <Michele.Faedi@studio.unibo.it>
2019-04-10 13:22:35 +0200

    Updated the test for the door in the map test

--- src/test/TestModel.java
+++ src/test/TestModel.java
@@ -62,21 +62,21 @@ public class TestModel {
     public void testMap() {
         boolean ok = false;
         ArrayList<Door> doors = new ArrayList<Door>();
-        doors.add(new Door(0, new DoorComponent(0, 1)));
-        doors.add(new Door(0, new DoorComponent(0, 2)));
+        doors.add(new Door(0, 1));
+        doors.add(new Door(0, 2));
         Room r1 = new RoomImpl(0, doors);
         ArrayList<Door> doorsTmp = new ArrayList<Door>();
-        doorsTmp.add(new Door(0, new DoorComponent(0, 1)));
-        doorsTmp.add(new Door(0, new DoorComponent(0, 2)));
+        doorsTmp.add(new Door(0, 1));
+        doorsTmp.add(new Door(0, 2));
         LinkedHashSet<Door> tmp = new LinkedHashSet<Door>(doorsTmp);
         assertEquals(r1.getDoor(), tmp);
 
         doors = new ArrayList<Door>();
-        doors.add(new Door(2, new DoorComponent(1, 0)));
+        doors.add(new Door(1, 0));
         Room r2 = new RoomImpl(1, doors);
 
         doors = new ArrayList<Door>();
-        doors.add(new Door(3, new DoorComponent(2, 0)));
+        doors.add(new Door(2, 0));
         Room r3 = new RoomImpl(2, doors);
 
         Floor floor = new FloorImpl(new ArrayList<Room>(Arrays.asList(r1, r2, r3)));
@@ -93,7 +93,7 @@ public class TestModel {
         assertTrue(ok);
 
         assertEquals(floor.getActiveRoom().getDoor(), new LinkedHashSet<Door>(Arrays.asList(
-                new Door(2, new DoorComponent(1, 0)))));
+                new Door(1, 0))));
 
         try {
             floor.changeRoom(2);
@@ -105,6 +105,6 @@ public class TestModel {
         assertTrue(ok);
 
         assertEquals(floor.getActiveRoom().getDoor(), new LinkedHashSet<Door>(Arrays.asList(
-                new Door(3, new DoorComponent(2, 0)))));
+                new Door(2, 0))));
     }
 }
