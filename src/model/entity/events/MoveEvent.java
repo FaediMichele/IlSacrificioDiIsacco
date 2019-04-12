a05a62ca42fac5ff5816ce04cba2adf5d6f58f31
Asia Lucchi <asialucchi@MacBook-Air-di-Asia.local>
2019-04-12 12:22:50 +0200

    MoveEvent created

---
+++ src/model/entity/events/MoveEvent.java
@@ -0,0 +1,28 @@
+package model.entity.events;
+
+import model.entity.Entity;
+
+/**
+ * Event that triggers the movement of the entity.
+ */
+public class MoveEvent extends AbstractEvent {
+
+    private double xMove;
+    private double yMove;
+    private double zMove;
+
+    /**
+ * 
+ * @param sourceEntity the entity source
+ * @param xMove the move on the x axis
+ * @param yMove the move on the y axis
+ * @param zMove the move on the z axis
+ */
+    public MoveEvent(final Entity sourceEntity, final double xMove, final double yMove, final double zMove) {
+        super(sourceEntity);
+        this.xMove = xMove;
+        this.yMove = yMove;
+        this.zMove = zMove;
+    }
+
+}
