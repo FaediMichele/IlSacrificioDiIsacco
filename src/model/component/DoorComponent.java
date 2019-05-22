485ff3b29f5d6a330a3090e273287c6b0083f773
Michele Faedi <Michele.Faedi@studio.unibo.it>
2019-05-22 12:32:09 +0200

    Completed the doorComponent, KeyChainComponent, Door; corrected a pdm problem in simpleheart.

--- src/model/component/DoorComponent.java
+++ src/model/component/DoorComponent.java
@@ -15,8 +15,7 @@ public class DoorComponent extends AbstractComponent<DoorComponent> {
 
     private final Integer destination;
     private final Integer location;
-    private final boolean hasPlayerPassed;
-    private final boolean locked;
+    private boolean hasPlayerPassed;
 
     /**
      * Create a door component with a destination room index.
@@ -24,23 +23,29 @@ public class DoorComponent extends AbstractComponent<DoorComponent> {
      * @param location The {@link Room} where the player is
      * @param destinationIndex index of the room
      * @param entity Entity that possess the component
-     * @param locked says if the door is locked
      */
-    public DoorComponent(final Entity entity, final Integer location, final Integer destinationIndex, final boolean locked) {
+    public DoorComponent(final Entity entity, final Integer location, final Integer destinationIndex) {
         super(entity);
         this.hasPlayerPassed = false;
         this.location = location;
         this.destination = destinationIndex;
-        this.locked = locked;
         entity.registerListener(new EventListener<CollisionEvent>() {
             @Override
             @Subscribe
             public void listenEvent(final CollisionEvent event) {
                 final CollisionEvent coll = (CollisionEvent) event;
                 if (coll.getSourceEntity().hasComponent(HealthComponent.class)) {
-                    if (locked && coll.getSourceEntity().hasComponent(KeychainComponent.class)) {
-                           // ((KeychainComponent) coll.getSourceEntity().getComponent(KeychainComponent.class).get()).
-                        System.out.println("TODO" + locked);
+                    final LockComponent lc = (LockComponent) getEntity().getComponent(LockComponent.class).get(); 
+                    final KeychainComponent kc = (KeychainComponent) coll.getSourceEntity().getComponent(KeychainComponent.class).get();
+                    if (lc != null && lc.isLocked() && kc != null) {
+                        if (kc.getNumKey() > 0) {
+                            lc.unlock();
+                            kc.removeKey();
+                        } else {
+                            return;
+                        }
+                    } else if (lc != null) {
+                        return;
                     }
                     coll.getSourceEntity().getRoom().getFloor()
                     .changeEntityRoom(coll.getSourceEntity(), location, destination);
@@ -57,7 +62,6 @@ public class DoorComponent extends AbstractComponent<DoorComponent> {
      * @return if the door is triggered
      */
     public boolean playerPassed() {
-        System.out.println("TODO" + locked);
         return this.hasPlayerPassed;
     }
 
@@ -75,6 +79,7 @@ public class DoorComponent extends AbstractComponent<DoorComponent> {
      */
     private void postPlayerPassed() {
         this.getEntity().postEvent(new DoorChangeEvent(getEntity()));
+        this.hasPlayerPassed = false;
     }
 
     /**
@@ -82,7 +87,7 @@ public class DoorComponent extends AbstractComponent<DoorComponent> {
      * 
      * @return the location
      */
-    public Integer getLocation() {
+    protected Integer getLocation() {
         return this.location;
     }
 
