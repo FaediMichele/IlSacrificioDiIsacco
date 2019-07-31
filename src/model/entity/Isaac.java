00cc7f447de3cf352a787266560083d58dca7717
Cristian Casadei <cristian.casadei6@studio.unibo.it>
2019-07-31 16:59:30 +0200

    Edit model player

--- src/model/entity/Isaac.java
+++ src/model/entity/Isaac.java
@@ -1,29 +1,47 @@
 package model.entity;
 
-import model.enumeration.BasicPlayerEnum;
-import model.enumeration.EntityEnum;
 
 /**
  * TODO.
  *
  */
 public class Isaac extends Player {
-    private static final EntityEnum ENTITY_NAME = BasicPlayerEnum.ISAAC;
-
-    /**
-     * TODO.
-     */
-    public Isaac() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public EntityEnum getNameEntity() {
-        return ENTITY_NAME;
-    }
+//    private static final EntityEnum ENTITY_NAME = BasicPlayerEnum.ISAAC;
+//    public static final double LIFE = 10; 
+//    public static final double SPEED = 10;
+//    public static final double DAMAGE = 10; 
+//    private static final Position DEFAULT_POSITION_VALUE = new Position(0.0, 0.0, 0.0);
+//    private static final double DEFAULT_SCALABLE_VALUE = 1;
+//    private static final int DEFAULT_WEIGHT_VALUE = 1;
+//    private static boolean stuck = false;
+//
+//    public Isaac() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    public EntityEnum getNameEntity() {
+//        return ENTITY_NAME;
+//    }
+//
+//    /**
+//     * 
+//     * @return stuck
+//     */
+//    public static boolean isSTUCK() {
+//        return stuck;
+//    }
+//
+//    /**
+//     * 
+//     * @param setStuck 
+//     */
+//    public static void setSTUCK(final boolean setStuck) {
+//        stuck = setStuck;
+//    }
 
 }
