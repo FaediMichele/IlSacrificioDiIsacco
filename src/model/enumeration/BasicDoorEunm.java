b43433ceabc998b53b0314ef33112cd481bb3e69
Cristian Casadei <cristian.casadei6@studio.unibo.it>
2019-08-08 11:05:39 +0200

    Add enum door

---
+++ src/model/enumeration/BasicDoorEunm.java
@@ -0,0 +1,15 @@
+package model.enumeration;
+
+/**
+ * 
+ * basic door enum.
+ *
+ */
+
+public enum BasicDoorEunm implements DoorEnum {
+
+    /**
+     * Basic type of door.
+     */
+   LEFT, RIGHT, UP, DOWN
+}
