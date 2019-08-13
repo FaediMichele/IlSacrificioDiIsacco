f8372b0cc514cef0a1033382193dd991b3ceb4b8
Andrea Pantieri <andrea.pantieri@studio.unibo.it>
2019-08-13 11:47:21 +0200

    Added bomb used avoiding multiple bomb per click

--- src/model/component/collision/DoorComponent.java
+++ src/model/component/collision/DoorComponent.java
@@ -68,7 +68,6 @@ public class DoorComponent extends LockCollisionComponent {
                 .changeEntityRoom(entity, location, destination);
             postPlayerPassed();
         }
-        
     }
 
     /**
