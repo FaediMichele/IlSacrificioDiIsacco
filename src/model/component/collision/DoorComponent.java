871ff6342ab903fffaa45507a4a78a2852f319a5
Andrea Pantieri <andrea.pantieri@studio.unibo.it>
2019-08-13 11:20:50 +0200

    Changed DoorComponent

--- src/model/component/collision/DoorComponent.java
+++ src/model/component/collision/DoorComponent.java
@@ -37,35 +37,38 @@ public class DoorComponent extends LockCollisionComponent {
      */
     @Override
     protected void afterUnlocks(final Entity entity) {
-        final BodyComponent eBody = entity.getComponent(BodyComponent.class).get();
-        final Entity otherDoor = getEntity().
-                getRoom().getFloor().getRooms()
-                .stream().filter(r -> r.getIndex() == this.destination).findFirst().get().getDoor().stream()
-                .filter(d -> d.getComponent(DoorComponent.class).get().destination.equals(location)).findFirst().get();
-        final BodyComponent otherBody = otherDoor.getComponent(BodyComponent.class).get();
-        switch ((BasicMovementEnum) otherDoor.getStatusComponent().getMove()) {
-            case DOWN:
-                eBody.setPosition(new Position(otherBody.getPosition().getX() + otherBody.getWidth() / 2 - eBody.getWidth() / 2,
-                        otherBody.getPosition().getY() - eBody.getHeight() - DISTANCE, eBody.getPosition().getZ()));
-                break;
-            case LEFT:
-                eBody.setPosition(new Position(otherBody.getPosition().getX() + eBody.getWidth() + DISTANCE,
-                        otherBody.getPosition().getY() - otherBody.getHeight() / 2 + eBody.getHeight(), eBody.getPosition().getZ()));
-                break;
-            case RIGHT:
-                eBody.setPosition(new Position(otherBody.getPosition().getX() - eBody.getWidth() - DISTANCE,
-                        otherBody.getPosition().getY() - otherBody.getHeight() / 2 + eBody.getHeight() / 2, eBody.getPosition().getZ()));
-                break;
-            case UP:
-                eBody.setPosition(new Position(otherBody.getPosition().getX() + otherBody.getWidth() / 2 - eBody.getWidth() / 2,
-                        otherBody.getPosition().getY() + otherBody.getHeight() + DISTANCE, eBody.getPosition().getZ()));
-                break;
-            default:
-                throw new IllegalStateException();
+        if (getEntity().getStatusComponent().getStatus() == BasicStatusEnum.OPEN) {
+            final BodyComponent eBody = entity.getComponent(BodyComponent.class).get();
+            final Entity otherDoor = getEntity().
+                    getRoom().getFloor().getRooms()
+                    .stream().filter(r -> r.getIndex() == this.destination).findFirst().get().getDoor().stream()
+                    .filter(d -> d.getComponent(DoorComponent.class).get().destination.equals(location)).findFirst().get();
+            final BodyComponent otherBody = otherDoor.getComponent(BodyComponent.class).get();
+            switch ((BasicMovementEnum) otherDoor.getStatusComponent().getMove()) {
+                case DOWN:
+                    eBody.setPosition(new Position(otherBody.getPosition().getX() + otherBody.getWidth() / 2 - eBody.getWidth() / 2,
+                            otherBody.getPosition().getY() - eBody.getHeight() - DISTANCE, eBody.getPosition().getZ()));
+                    break;
+                case LEFT:
+                    eBody.setPosition(new Position(otherBody.getPosition().getX() + eBody.getWidth() + DISTANCE,
+                            otherBody.getPosition().getY() - otherBody.getHeight() / 2 + eBody.getHeight(), eBody.getPosition().getZ()));
+                    break;
+                case RIGHT:
+                    eBody.setPosition(new Position(otherBody.getPosition().getX() - eBody.getWidth() - DISTANCE,
+                            otherBody.getPosition().getY() - otherBody.getHeight() / 2 + eBody.getHeight() / 2, eBody.getPosition().getZ()));
+                    break;
+                case UP:
+                    eBody.setPosition(new Position(otherBody.getPosition().getX() + otherBody.getWidth() / 2 - eBody.getWidth() / 2,
+                            otherBody.getPosition().getY() + otherBody.getHeight() + DISTANCE, eBody.getPosition().getZ()));
+                    break;
+                default:
+                    throw new IllegalStateException();
+            }
+            entity.getRoom().getFloor()
+                .changeEntityRoom(entity, location, destination);
+            postPlayerPassed();
         }
-        entity.getRoom().getFloor()
-            .changeEntityRoom(entity, location, destination);
-        postPlayerPassed();
+        
     }
 
     /**
