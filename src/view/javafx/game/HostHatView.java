b80f957addc8a1a70499e536bf9fdf4808cc624a
Andrea Pantieri <andrea.pantieri@studio.unibo.it>
2019-08-16 14:20:42 +0200

    Added upgrades views

---
+++ src/view/javafx/game/HostHatView.java
@@ -0,0 +1,39 @@
+package view.javafx.game;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+/**
+ * View for the {@link HostHat}.
+ *
+ */
+public class HostHatView extends AbstractEntityView {
+    private static Image img;
+
+    static {
+        try {
+            final BufferedImage image = ImageIO.read(
+                    new File(HostHatView.class.getResource("/gameImgs/upgradesImgs/collectibles_375_hosthat.png").toURI()));
+            img = SwingFXUtils.toFXImage(image, null);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Draws the image.
+     */
+    @Override
+    public void draw(final GraphicsContext gc) {
+        gc.drawImage(img, super.getX(), super.getY(), super.getWidth(), super.getHeight());
+    }
+
+}
