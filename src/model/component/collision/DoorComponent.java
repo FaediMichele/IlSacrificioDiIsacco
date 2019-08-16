10d405e22555b3ef00c13656e85568a93969c820
Cristian Casadei <cristian.casadei6@studio.unibo.it>
2019-08-16 12:55:21 +0200

    Testing door

--- src/model/component/collision/DoorComponent.java
+++ src/model/component/collision/DoorComponent.java
@@ -1,14 +1,19 @@
 package model.component.collision;
 
+import java.util.Optional;
+
 import model.component.BodyComponent;
+import model.entity.Door;
 import model.entity.Entity;
 import model.enumeration.BasicMovementEnum;
 import model.enumeration.BasicStatusEnum;
 import model.events.DoorChangeEvent;
 import model.util.Position;
+import util.Pair;
+
 /**
  * 
- *Component that manages the collision of the door.
+ * Component that manages the collision of the door.
  */
 public class DoorComponent extends LockCollisionComponent {
     private static final int DISTANCE = 10;
@@ -19,10 +24,10 @@ public class DoorComponent extends LockCollisionComponent {
     /**
      * Create a door component with a destination room index.
      * 
-     * @param locationIndex The {@link Room} where the player is
+     * @param locationIndex    The {@link Room} where the player is
      * @param destinationIndex index of the room
-     * @param entity Entity that possess the component
-     * @param status the status of the door.
+     * @param entity           Entity that possess the component
+     * @param status           the status of the door.
      */
     public DoorComponent(final Entity entity, final Integer locationIndex, final Integer destinationIndex,
             final BasicStatusEnum status) {
@@ -38,33 +43,52 @@ public class DoorComponent extends LockCollisionComponent {
     @Override
     public void afterUnlocks(final Entity entity) {
         final BodyComponent eBody = entity.getComponent(BodyComponent.class).get();
-        final Entity otherDoor = getEntity().
-                getRoom().getFloor().getRooms()
-                .stream().filter(r -> r.getIndex() == this.destination).findFirst().get().getDoor().stream()
-                .filter(d -> d.getComponent(DoorComponent.class).get().destination.equals(location)).findFirst().get();
+        System.out.println("destinazione = "  + this.destination + " size rooms = " + entity.getRoom().getFloor().getRooms().size());
+        System.out.println("stanza di partenza " + getEntity().getRoom().getIndex() + " = " + this.location);
+        System.out.println("size porte destinazione = "  +  entity.getRoom().getFloor().getRooms().get(this.destination).getDoor().size());
+
+        getEntity().getRoom().getFloor().getRooms().get(this.destination).getDoor()
+        .stream()
+        .map(d -> d.getComponent(DoorComponent.class).get())
+        .map(dc -> new Pair<Integer, Integer>(dc.getLocation(), dc.getDestination()))
+        .forEach(p -> {
+            System.out.println("Location = " + p.getX());
+            System.out.println("Destination = " + p.getY());
+        });
+
+        final Optional<? extends Door> optOtherDoor = getEntity().getRoom().getFloor()
+                .getRooms().get(this.destination)
+                .getDoor().stream()
+                .filter(d -> d.getComponent(DoorComponent.class).get().getDestination().equals(this.location))
+                .findFirst();
+        System.out.println(optOtherDoor.isPresent());
+        final Entity otherDoor = optOtherDoor.get();
         final BodyComponent otherBody = otherDoor.getComponent(BodyComponent.class).get();
         switch ((BasicMovementEnum) otherDoor.getStatusComponent().getMove()) {
-            case DOWN:
-                eBody.setPosition(new Position(otherBody.getPosition().getX() + otherBody.getWidth() / 2 - eBody.getWidth() / 2,
-                        otherBody.getPosition().getY() + otherBody.getHeight() + DISTANCE, eBody.getPosition().getZ()));
-                break;
-            case LEFT:
-                eBody.setPosition(new Position(otherBody.getPosition().getX() + otherBody.getWidth() + DISTANCE,
-                        otherBody.getPosition().getY() - otherBody.getHeight() / 2 + eBody.getHeight(), eBody.getPosition().getZ()));
-                break;
-            case RIGHT:
-                eBody.setPosition(new Position(otherBody.getPosition().getX() - eBody.getWidth() - DISTANCE,
-                        otherBody.getPosition().getY() + otherBody.getHeight() / 2 - eBody.getHeight() / 2, eBody.getPosition().getZ()));
-                break;
-            case UP:
-                eBody.setPosition(new Position(otherBody.getPosition().getX() + otherBody.getWidth() / 2 - eBody.getWidth() / 2,
-                        otherBody.getPosition().getY() - eBody.getHeight() - DISTANCE, eBody.getPosition().getZ()));
-                break;
-            default:
-                throw new IllegalStateException();
+        case DOWN:
+            eBody.setPosition(new Position(
+                    otherBody.getPosition().getX() + otherBody.getWidth() / 2 - eBody.getWidth() / 2,
+                    otherBody.getPosition().getY() + otherBody.getHeight() + DISTANCE, eBody.getPosition().getZ()));
+            break;
+        case LEFT:
+            eBody.setPosition(new Position(otherBody.getPosition().getX() + otherBody.getWidth() + DISTANCE,
+                    otherBody.getPosition().getY() - otherBody.getHeight() / 2 + eBody.getHeight(),
+                    eBody.getPosition().getZ()));
+            break;
+        case RIGHT:
+            eBody.setPosition(new Position(otherBody.getPosition().getX() - eBody.getWidth() - DISTANCE,
+                    otherBody.getPosition().getY() + otherBody.getHeight() / 2 - eBody.getHeight() / 2,
+                    eBody.getPosition().getZ()));
+            break;
+        case UP:
+            eBody.setPosition(
+                    new Position(otherBody.getPosition().getX() + otherBody.getWidth() / 2 - eBody.getWidth() / 2,
+                            otherBody.getPosition().getY() - eBody.getHeight() - DISTANCE, eBody.getPosition().getZ()));
+            break;
+        default:
+            throw new IllegalStateException();
         }
-        entity.getRoom().getFloor()
-            .changeEntityRoom(entity, location, destination);
+        entity.getRoom().getFloor().changeEntityRoom(entity, location, destination);
         postPlayerPassed();
     }
 
